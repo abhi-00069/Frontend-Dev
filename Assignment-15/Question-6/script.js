2123c51de2cc1de0b6e3181cb511d979f35b026e
abhi-00069 <abhisheksharma41324@gmail.com>
2025-11-24 10:40:10 +0530

    Create script.js

---
+++ Assignment-15/Question-6/script.js
@@ -0,0 +1,31 @@
+function Person(name) {
+    this.name = name;
+}
+Person.prototype.showName = function() {
+    console.log(this.name);
+};
+
+function Faculty(name, department) {
+    Person.call(this, name);
+    this.department = department;
+}
+Faculty.prototype = Object.create(Person.prototype);
+Faculty.prototype.constructor = Faculty;
+Faculty.prototype.showDepartment = function() {
+    console.log(this.department);
+};
+
+function Professor(name, department, subject) {
+    Faculty.call(this, name, department);
+    this.subject = subject;
+}
+Professor.prototype = Object.create(Faculty.prototype);
+Professor.prototype.constructor = Professor;
+Professor.prototype.showSubject = function() {
+    console.log(this.subject);
+};
+
+const prof = new Professor("Abhishek", "CSE", "AI");
+prof.showName();
+prof.showDepartment();
+prof.showSubject();
