1c3a5b11311f374abdb3eedcd1c718fd988d4301
abhi-00069 <abhisheksharma41324@gmail.com>
2025-11-25 10:31:35 +0530

    Create q7.js

---
+++ Assignment-17/Question-7/q7.js
@@ -0,0 +1,20 @@
+const box = document.getElementById('box');
+const coords = document.getElementById('coords');
+
+box.addEventListener('mousemove', (e) => {
+  const rect = box.getBoundingClientRect();
+  const x = Math.round(e.clientX - rect.left);
+  const y = Math.round(e.clientY - rect.top);
+  coords.textContent = `x: ${x} , y: ${y}`;
+});
+
+box.addEventListener('dblclick', (e) => {
+  const rect = box.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+  const dot = document.createElement('div');
+  dot.className = 'dot';
+  dot.style.left = `${x}px`;
+  dot.style.top = `${y}px`;
+  box.appendChild(dot);
+});
